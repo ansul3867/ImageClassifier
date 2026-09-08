f091735000ad72183f073f9db8ad55e08f22b8cd
Ansul <ansul3867@icloud.com>
2026-09-08 19:26:03 +0530

    Initialization of separate files for collaborative working

---
+++ main.py
@@ -0,0 +1,5 @@
+## Data Pre-Processing Pipeline
+
+## Process of How we will be doing things is explained here.
+# Instead of choosing ML, we'll be doing DL. It might give us a bit of complications and more effort towrds studying for the viva, but for images, we need to use DL.
+# As of now, I'm deciding to carry on with this only. If we face any issue, we can change our architecture in near future.
